40814455602ce8ec78b40f8e591057b90d0d8a07
Terje Russka <terjerusska@gmail.com>
2018-03-31 19:30:38 +0300

    Add cake statistics second table

--- cake-shop/src/main/resources/public/js/statistics.js
+++ cake-shop/src/main/resources/public/js/statistics.js
@@ -4,27 +4,6 @@ $( document ).ready(function() {
         return $.get('/orders/cakes/all');
     };
 
-    const getCakeNames = function (orderCakes) {
-        const cakes = [];
-        const cakeRows = $.map(orderCakes, function (orderCake) {
-            if (!cakes.includes(orderCake.cake.name)) {
-                cakes.push(orderCake.cake.name);
-            }
-        });
-        return cakes;
-    };
-
-    const getCakeAmounts = function (orderCakes) {
-        const amounts = [];
-        const cakeRows = $.map(orderCakes, function (orderCake) {
-            if (!cakes.includes(orderCake.cake.name)) {
-                const value =
-                cakes.push(orderCake.cake.name);
-            }
-        });
-        return cakes;
-    };
-
     const sortCakeData = function (orderCakes) {
         const dataArray = {};
         const cakeRows = $.map(orderCakes, function (orderCake) {
@@ -37,7 +16,7 @@ $( document ).ready(function() {
         return dataArray;
     }
 
-    const showStatistics = function (orderCakes){
+    const showStatisticsChart = function (orderCakes){
         var ctx = document.getElementById("cakeChart").getContext('2d');
         const dataArray = sortCakeData(orderCakes);
         const data = Object.values(dataArray);
@@ -61,32 +40,68 @@ $( document ).ready(function() {
                 },
                 title: {
                   display: true,
-                  text: 'Total amount of ordered cakes'
+                  text: 'Percentage of each cake ordered'
                 },
                 animation: {
                   animateScale: true,
                   animateRotate: true
                 },
                 tooltips: {
-                  callbacks: {
-                    label: function(tooltipItem, data) {
-                    	var dataset = data.datasets[tooltipItem.datasetIndex];
-                      var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
-                        return previousValue + currentValue;
-                      });
-                      var currentValue = dataset.data[tooltipItem.index];
-                      var precentage = Math.floor(((currentValue/total) * 100)+0.5);
-                      return precentage + "%";
+                    callbacks: {
+                        label: function(tooltipItem, data) {
+                            var dataset = data.datasets[tooltipItem.datasetIndex];
+                            var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                                return previousValue + currentValue;
+                            });
+                            var currentValue = dataset.data[tooltipItem.index];
+                            var currentLabel = data.labels[tooltipItem.index];
+                            var precentage = Math.floor(((currentValue/total) * 100)+0.5);
+                            return currentLabel + ": " + precentage + "%";
+                        }
                     }
-                  }
                 }
             }
         });
     }
 
+    const showStatisticsTable = function (orderCakes){
+            var ctx = document.getElementById("cakeTable").getContext('2d');
+            const dataArray = sortCakeData(orderCakes);
+            const data = Object.values(dataArray);
+            const labels = Object.keys(dataArray);
+            const generatedColours = data.map((value, index) => index % 2 ? '#343a40' : '#dc3545');
+
+            var myChart = new Chart(ctx, {
+                type: 'horizontalBar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        data: data,
+                        borderColor: generatedColours,
+                        backgroundColor: generatedColours,
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                      display: true,
+                      text: 'Total amount of each cake ordered'
+                    },
+                    animation: {
+                      animateScale: true,
+                      animateRotate: true
+                    }
+                }
+            });
+        }
+
     const refreshOrderCakeData = function () {
         loadOrderCakeData().then(function (orderCakes) {
-            showStatistics(orderCakes);
+            showStatisticsChart(orderCakes);
+            showStatisticsTable(orderCakes);
         });
     };
 
